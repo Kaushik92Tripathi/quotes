5f160d5fa6b84c585efb43a6a29cc06168c60e5e
Kaushik92Tripathi <kaushik.tripathi@tothenew.com>
2025-02-25 16:19:33 +0530

    Initial commit from Create Next App

---
+++ src/contexts/LoadingContext.js
@@ -0,0 +1,19 @@
+// File: src/contexts/LoadingContext.js
+import { createContext, useContext, useState } from 'react';
+
+const LoadingContext = createContext({
+  isLoading: false,
+  setIsLoading: () => {},
+});
+
+export const LoadingProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = () => useContext(LoadingContext);
